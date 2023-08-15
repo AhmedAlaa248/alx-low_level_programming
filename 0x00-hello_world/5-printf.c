4286373bd5a187ddcf27e2a8d2aa14661e96ecdf
AhmedAlaa248 <ahmed.elrweney@yahoo.com>
2023-08-14 17:51:10 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- * main - Point entery
+ * main -Entery point
+ * 
+ * Description: A C program that with printf function 
+ *
+ * Return: Always 0 (Success)
 */
 
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
