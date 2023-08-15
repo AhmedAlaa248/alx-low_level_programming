ff2b25308caf143f36efb7f8878fdf6dc35690b4
AhmedAlaa248 <ahmed.elrweney@yahoo.com>
2023-08-14 19:32:40 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: A C program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: A C program that prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+
+	while (a < 'z')
+	{
+		putchar(a);
+		a++
+	}
+
+	while (A < 'Z')
+	{
+		putchar(A);
+		A++
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
