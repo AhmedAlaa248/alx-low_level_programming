45a203c359edee455a8ef3331d63e8e6a142145c
AhmedAlaa248 <ahmed.elrweney@yahoo.com>
2023-08-14 19:58:29 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: A C program that 
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	int i = 0;
+	
+	while (i <= 9)
+	{
+		putchar(i + 48);
+		
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
