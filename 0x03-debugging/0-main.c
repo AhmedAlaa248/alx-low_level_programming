eec7eb43bd7debe75069cb28d95634f4b1df9a49
osc <osc@pop-os.localdomain>
2023-08-18 21:01:58 +0200

    done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,21 +6,21 @@
 */
 
 
-int positive_or_negative(int) {
-	if( n > 0)
-		printf("%i is possitive\n", n);
-	else if( n < 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
-}
+
+
+
+
+
+
+
+
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
