6959a6bac8349ff9abdfb32e2af18480a15820f3
osc <osc@pop-os.localdomain>
2023-08-19 04:07:10 +0200

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - function that prints the alphabet in lowercase
+ *
+ * @n : checks the input
+ *
+ * Return: returns 0
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n >= 0)
+		for (i = 0; i < n; i++)
+		{
+		_putchar('92');
+		if (i > 0)
+			_putchar('\n');
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+		}
+}
