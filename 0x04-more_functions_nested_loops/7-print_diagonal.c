fc1e03ed9f7b4dbc82c0c500c82e8aece274d607
osc <osc@pop-os.localdomain>
2023-08-19 04:15:32 +0200

    done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,13 @@ void print_diagonal(int n)
 	if (n >= 0)
 		for (i = 0; i < n; i++)
 		{
-		_putchar(92);
-		if (i > 0)
-			_putchar('\n');
-			for (j = 0; j <= i; j++)
-				_putchar(' ');
+			_putchar(92);
+			if (i > 0)
+			{
+				_putchar('\n');
+				for (j = 0; j <= i; j++)
+					_putchar(' ');
+			}
 		}
 	_putchar('\n');
 }
