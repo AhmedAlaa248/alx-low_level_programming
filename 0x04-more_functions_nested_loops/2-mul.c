03df777add94bcff1d9775fd4d404834043632df
osc <osc@pop-os.localdomain>
2023-08-19 03:06:21 +0200

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - function that prints the alphabet in lowercase
+ *
+ * @a : checks input of function
+ * @b : checks input of function
+ *
+ * Return: returns 1 if 'c' is digit
+*/
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - function that prints the alphabet in lowercase
+ *
+ * @a : checks input of function
+ * @b : checks input of function
+ *
+ * Return: returns 1 if 'c' is digit
+*/
+
+void print_numbers(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + 48);
+
+	_putchar('\n');
+}
