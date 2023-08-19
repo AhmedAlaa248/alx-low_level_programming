0883c99b890235ee2cc3d667cb267ecfda79a6fc
osc <osc@pop-os.localdomain>
2023-08-19 03:21:10 +0200

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_more_numbers - function that prints the alphabet in lowercase
+ *
+ * Return: returns 0
+*/
+
+void print_more_numbers(void)
+{
+
+	int i, j;
+
+	for (j = 0; j < 11; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + 48);
+		}
+		_putchar('\n');
+	}
+
+}
