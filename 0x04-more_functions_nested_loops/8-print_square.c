77a28783a3d0911f6ad2dc03864d749a7e052a07
osc <osc@pop-os.localdomain>
2023-08-19 04:35:11 +0200

    done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,15 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 				_putchar('#');
-			if (i != size - 1)
-				_putchar('\n');
+
+			_putchar('\n');
 		}
-	_putchar('\n');
+
 }
