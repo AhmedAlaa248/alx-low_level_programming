4cbe2fa74cf335d9ef0781ba70aa51799d09e79b
osc <osc@pop-os.localdomain>
2023-08-19 03:47:45 +0200

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,21 @@
 void print_more_numbers(void)
 {
 
-	int i, j;
+	int i, j, currentNum;
 
 	for (j = 0; j < 11; j++)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			_putchar(i + 48);
+			currentNum = i;
+			if (i > 9)
+			{
+				_putchar(1 + 48);
+				currentNum = i % 10;
+			}
+			_putchar(currentNum + 48);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 
 }
