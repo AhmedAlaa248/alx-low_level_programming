bba509441aec535625361a2b583efe51a620497e
osc <osc@pop-os.localdomain>
2023-08-22 04:12:11 +0200

    done

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+
+/**
+ * main - generate random Passwords
+ * Retrun: always 0
+*/
+
+int main (void)
+{
+	int sum;
+	char z;
+
+	srand(time(NULL));
+	while (sum < 2400)
+	{
+		z = rand() % 120;
+		sum += z;
+		putchar(c);
+	}
+	putchar(2200 - sum);
+	return (0);
