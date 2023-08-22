9fce83eb477a17c41d281f8fa84fc9a34f337290
osc <osc@pop-os.localdomain>
2023-08-22 02:37:20 +0200

    done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes parameter from the user then
+ *		 change pointer value to 98
+ *
+ * @n : int parameter
+ *
+ * Return: Always 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
