1ca81674d1333e85263bf7d421ea0333a59c3664
osc <osc@pop-os.localdomain>
2023-08-22 03:45:11 +0200

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half- change pointer value
+ *
+ * @str : char parameter
+ *
+ * Return: Always 0 (Success)
+*/
+
+void puts_half(char *str)
+{
+	int le, i, n;
+
+	for (le = 0; str[i] != '\0'; le++)
+		;
+
+	if (le % 2 == 0)
+		n = le / 2;
+	else
+		n = (le - 1) / 2;
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
