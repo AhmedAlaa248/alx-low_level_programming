041dcaba2034d563f83377f51db0df505ddc5b47
osc <osc@pop-os.localdomain>
2023-08-29 03:38:12 +0200

    done

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - change chessboard
+ *
+ * @a : char parameter
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int itr, j;
+
+	for (itr = 0; itr < 8; itr++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[itr][j]);
+		_putchar('\n');
+	}
+}
+
