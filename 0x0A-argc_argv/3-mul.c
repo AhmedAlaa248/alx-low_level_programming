53cf6dce24d27b71e95d9a4d4259292816070a5a
osc <osc@pop-os.localdomain>
2023-09-02 05:28:45 +0200

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - change pointer value
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * @argc : char parameter
- * @argv : j
- *
- * Return: Always 0 (Success)
-*/
-
-int main(int argc, char *argv[])
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
 {
-	int i, d, n, l, f, dig;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
 	n = 0;
-	l = 0;
+	len = 0;
 	f = 0;
-	dig = 0;
+	digit = 0;
 
-	while (s[l] != '\0')
-		l++;
+	while (s[len] != '\0')
+		len++;
 
-	while (i < l && f == 0)
+	while (i < len && f == 0)
 	{
-		if(s[i] == '-')
-			d++;
+		if (s[i] == '-')
+			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			dig = s[i] - '0';
-
+			digit = s[i] - '0';
 			if (d % 2)
-				dig = -dig;
-
-			n = n * 10 + dig;
+				digit = -digit;
+			n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
@@ -51,3 +46,28 @@ int main(int argc, char *argv[])
 	return (n);
 }
 
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
