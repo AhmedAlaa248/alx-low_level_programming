ced6ff5527824241a21a28712ce387587824a366
osc <osc@pop-os.localdomain>
2023-09-09 03:14:33 +0200

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,39 @@
-#include"main.h"
+#include "holberton.h"
 #include <stdlib.h>
 /**
- *  *string_nconcat- a function that concatenates two strings.
- * @s1: arguments
- * @s2: arguments
- * @n: arguments
-* Return: zeft teen
-*/
-
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: limit of s2
+ * Return: pointer to new space in memory or null
+ **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, l1 = 0, l2 = 0;
+	char *strDup;
+	int i;
+	unsigned int j;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	strDup = malloc(sizeof(char) * (i + n + 1));
+	if (strDup == NULL)
 		return (NULL);
-
-	while (s1 && s1[l1])
-		l1++;
-
-	while (s2 && s2[l2])
-		l2++;
-
-	if (n < l2)
-		s = malloc(sizeof(char) * (l1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (l1 + l2 + 1));
-
-	if (!s)
-		return (NULL);
-
-	while (i < l1)
+	i = j = 0;
+	while (s1[i] != '\0')
 	{
-		s[i] = s1[i];
+		strDup[i] = s1[i];
 		i++;
 	}
-
-	while (n < l2 && i < (l1 + n))
+	while (j < n && s2[j] != '\0')
 	{
-		s[i++] = s2[j++];
+		strDup[i] = s2[j];
+		i++, j++;
 	}
-
-	while (n >= l2 && i < (l1 + l2))
-	{
-		s[i++] = s2[j++];
-	}
-
-	s[i] = '\0';
-	return (s);
+	strDup[i] = '\0';
+	return (strDup);
 }
