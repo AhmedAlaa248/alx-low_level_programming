f073e86210a738013986183ba11beb870bb5f398
osc <osc@pop-os.localdomain>
2023-09-09 02:59:48 +0200

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - change pointer value
+ *
+ * @b : int parameter
+ *
+ * Return: Always 0 (Success)
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *z;
+
+	z = malloc(b);
+
+	if (x == 0)
+		exit(98);
+
+	return (z);
+}
