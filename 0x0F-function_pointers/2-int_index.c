48d9586fde59e74d538e810584d44873edc94052
osc <osc@pop-os.localdomain>
2023-09-14 01:16:59 +0200

    done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - change pointer value
+ *
+ * @array : int array parameter
+ * @size : array size
+ * @cmp : void function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++
+		}
+	return (-1);
+}
