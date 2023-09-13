a8dd8b1029735236bdc6beb6ba77b412212f12bd
osc <osc@pop-os.localdomain>
2023-09-14 00:52:33 +0200

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_name - change pointer value
+ *
+ * @name : char parameter
+ * @f : the printing func
+ * @* : char parameter
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
