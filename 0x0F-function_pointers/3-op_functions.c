866a5ed1e2fe69ce486ebbacee0120fd0d547d60
osc <osc@pop-os.localdomain>
2023-09-14 01:33:58 +0200

    done

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+/**
+ * op_add - add two integers
+ *
+ * @a : int parameter
+ * @b : int parameter
+ *
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - substruct two integers
+ *
+ * @a : int parameter
+ * @b : int parameter
+ *
+ * Return: the diffrence
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - two integers
+ *
+ * @a : int parameter
+ * @b : int parameter
+ *
+ * Return: the multiplication
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - two integers
+ *
+ * @a : int parameter
+ * @b : int parameter
+ *
+ * Return: the
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - two integers
+ *
+ * @a : int parameter
+ * @b : int parameter
+ *
+ * Return: the
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
