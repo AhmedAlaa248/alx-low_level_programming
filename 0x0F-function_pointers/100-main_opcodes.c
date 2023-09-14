70dfd4019be2d51c5fa52ef5876a29bfc57fd22d
osc <osc@pop-os.localdomain>
2023-09-14 03:14:35 +0200

    done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - change pointer value
+ *
+ * @argc : char parameter
+ * @argv : j
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1])
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+
+	return (0);
+}
