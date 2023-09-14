a0054e9c6193f6989f1110a3f6ff223cb2fd8bc9
osc <osc@pop-os.localdomain>
2023-09-15 00:14:28 +0200

    done

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - change pointer value
+ *
+ * @n : counter parameter
+ * @separator : string parameter
+ * @...: the integers to sum
+ *
+ * Return: the integer
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	
+	unsigned int i = n;
+	va_list list;
+
+	va_start(list, n);
+
+
+	while (i--)
+		printf("%d%s ", va_arg(list, const unsigned int),
+				i ? (separator ? separator : "") : "\n");
+	
+	va_end(list);
+
+}
