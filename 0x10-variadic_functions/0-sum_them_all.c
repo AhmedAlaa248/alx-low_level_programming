c48920bd2b91656a1bc143c48bce4f95d412ad12
osc <osc@pop-os.localdomain>
2023-09-14 23:30:54 +0200

    done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
+	{
+		if (n != 0)
+			sum += va_arg(valist, const unsigned int);
+		else
+			return (0);
+	}
 	va_end(valist);
 	return (sum);
 }
