d241f388e092c7bebefda8562793d10db4773d90
osc <osc@pop-os.localdomain>
2023-09-15 00:44:26 +0200

    done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,7 @@
  *
  * @n : counter parameter
  * @...: the integers to sum
- * @ seperator : char parameter
+ * @separator : char parameter
  * Return: the integer
 */
 
@@ -13,4 +13,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 
 	int i = n;
+	char *str;
+	va_list list;
+
+	va_start(list, n);
+
+	while (i--)
+		printf("%s%s", (str = va_arg(list, char*)) ? str : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+
+	va_end(list);
 }
