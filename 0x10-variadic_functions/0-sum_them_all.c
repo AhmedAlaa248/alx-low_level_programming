ff1cc63f580319d8e162e642dabd99d77d897414
osc <osc@pop-os.localdomain>
2023-09-14 23:26:12 +0200

    done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,21 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
- * sum_them_all - change pointer value
+ * sum_them_all - sums of all its parameters.
+ * @n: number of parameters
  *
- * @n : counter parameter
- * @...: the integers to sum
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: sum of all parameters
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list a;
-	unsigned int sum = 0;
+	int sum = 0;
 	unsigned int i;
+	va_list valist;
 
-	if (n == 0)
-		return (0);
-
-	va_start(a, n);
-
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(a, int);
-
-	va_end(a);
+		sum += va_arg(valist, int);
+	va_end(valist);
 	return (sum);
 }
-
