ef3c1ecaa0506faed4e532e437de1678d27f7df5
osc <osc@pop-os.localdomain>
2023-09-14 23:04:57 +0200

    done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,10 @@
-#include ".variadic_functions.h"
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - change pointer value
  *
  * @n : counter parameter
+ * @...: the integers to sum
  *
  * Return: Always 0 (Success)
 */
@@ -19,12 +20,9 @@ int sum_them_all(const unsigned int n, ...);
 	va_start(a, n);
 
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(a, int);
-	}
 
 	va_end(a);
-
 	return (sum);
 }
 
